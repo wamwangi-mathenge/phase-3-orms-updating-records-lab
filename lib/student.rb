ea11f45c9d6acf4b35f6a4bc39ccbdd04131defc
Brian <wamwangimathenge@gmail.com>
2023-02-22 14:26:37 +0300

    .create_table creates the students table with columns that match the attributes

--- lib/student.rb
+++ lib/student.rb
@@ -5,5 +5,25 @@ class Student
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
 
+  attr_accessor :name, :grade, :id
+
+  def initialize(name:, grade:, id: nil)
+    @id = id
+    @name = name
+    @grade = grade
+  end
+
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS students (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        grade TEXT
+      )
+      SQL
+
+      DB[:conn].execute(sql)
+  end
+
 
 end
