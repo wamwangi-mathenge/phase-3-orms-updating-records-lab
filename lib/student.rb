2f7d84d2e42d0a2fd02a62f2038107aa05182094
Brian <wamwangimathenge@gmail.com>
2023-02-22 14:33:22 +0300

    .drop_table responsible for droppint the students table

--- lib/student.rb
+++ lib/student.rb
@@ -22,5 +22,13 @@ class Student
       DB[:conn].execute(sql)
   end
 
+  def self.drop_table
+    sql = <<-SQL
+      DROP TABLE IF EXISTS students
+    SQL
+
+    DB[:conn].execute(sql)
+  end
+
 
 end
