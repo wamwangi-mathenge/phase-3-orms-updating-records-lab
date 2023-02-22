c9d2962550177244dfdaa606fdfbfc8fa6dbefff
Brian <wamwangimathenge@gmail.com>
2023-02-22 14:31:21 +0300

    The #initialize method takes in id, name and grade

--- lib/student.rb
+++ lib/student.rb
@@ -2,15 +2,12 @@ require_relative "../config/environment.rb"
 
 class Student
 
-  # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]
-
   attr_accessor :name, :grade, :id
 
-  def initialize(name:, grade:, id: nil)
-    @id = id
-    @name = name
-    @grade = grade
+  def initialize(name, grade, id = nil)
+      @id = id
+      @name = name
+      @grade = grade
   end
 
   def self.create_table
